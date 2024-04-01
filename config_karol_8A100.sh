6884485fea8479abb9194507d60444635439b88c
Petros Anastasiadis <panastas@cslab.ece.ntua.gr>
2024-04-01 09:51:59 +0200

    Updated plotters

--- config_karol_8A100.sh
+++ config_karol_8A100.sh
@@ -24,13 +24,13 @@ export PARALIA_NUM_DEVICES=8
 # CHECKME: Define cuda architecture (Tesla K40 = 35, GTX 1060/70 = 61,) P100 = 60, V100 = 70, A100 = 80
 export PARALIA_CUDA_ARCH=80
 
-# CHECKME: Define cuda toolkit path. If left 'default' CHANGELINK will try to use the default compiler calls without prefix(es)
+# CHECKME: Define cuda toolkit path. Use "default" to use the default compiler calls without prefix(es)
 export PARALIA_CUDA_TOOLKIT_PREFIX="/usr/local/cuda-12.2"
 
 # CHECKME: Define cuda load command (e.g. should always be '-lcuda', but its not! If you don't know what to do, leave it)
 export PARALIA_CUDA_LOAD_COMMAND='-lcuda'
 
-# CHECKME: Define gcc compiler path. If left 'default' CHANGELINK will try to use the default compiler calls without prefix(es)
+# CHECKME: Define gcc compiler path. Use "default" to use the default compiler calls without prefix(es)
 export PARALIA_CXX_PREFIX="default"
 
 # CHECKME: Define path for prebuild openblas. NOTE: OpenBLAS built using the same gcc is adviced.
@@ -39,7 +39,7 @@ export PARALIA_OPENBLAS_PREFIX="/apps/modules/numlib/OpenBLAS/"
 # CHECKME: Define path for prebuild boost. NOTE: boost built using the same gcc is adviced.
 export PARALIA_BOOST_PREFIX="/apps/all/Boost/1.81.0-GCC-12.2.0"
 
-# CHECKME: Define the directory CHANGELINK will be installed in. 'default' = buildir/${system}-install
+# CHECKME: Define the directory the project will be installed in. 'default' = buildir/${system}-install
 export PARALIA_INSTALL_PREFIX="default"
 
 # CHECKME: Define the Watt for your CPU - no CPU power tool used currently.
